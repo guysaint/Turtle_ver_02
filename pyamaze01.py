58b6f6bfbaeb8c94201885b3f2d5793fe26a35e3
guysaint <guysaint@naver.com>
2025-07-22 15:04:19 +0900

    미로찾기 튜토리얼 완성
    미로찾기 튜토리얼 완성

--- pyamaze01.py
+++ pyamaze01.py
@@ -1,6 +1,8 @@
 from pyamaze import maze, COLOR, agent
 m = maze(10,10)
-m.CreateMaze(theme=COLOR,white)
-m.CreateMaze(loadMaze=maze--2025-07-22--14-52-02.csv)
-a=agent(m, shape='arrow', footprints=True)
+#m.CreateMaze(theme=COLOR.light)
+m.CreateMaze(loadMaze="maze--2025-07-22--14-52-02.csv")
+a=agent(m, filled=True, footprints=True)
+m.tracePath({a:m.path})
 m.run()
+print(m.maze_map)
