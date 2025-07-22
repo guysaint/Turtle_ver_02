71341f174ae4de7a3cf9a48a5de316e9df12e361
guysaint <guysaint@naver.com>
2025-07-22 15:50:35 +0900

    스켈레톤 2
    함수 뼈대 만들기

--- pyamaze01.py
+++ pyamaze01.py
@@ -10,6 +10,23 @@ m.tracePath({a:m.path})
 m.run()
 #벽 따라가기 알고리즘
 
+def wall_following_algorithm(maze_obj):
+    
+    #특정 방향으로 이동 할 수 있는지 판단
+    def can_move_to(from_pos, direction):
+    
+    
+    #현재 위치에서 특정 방향으로 이동했을 때 다음 위치를 반환
+    def get_next_position(pos, direction):    
+    
+    #현재 위치에서 목표지점까지 직진이 가능한지 확인
+    def can_go_to_goal_directly():
+    
+    #오른손 법칙에 따라 이동 방향 결정
+    def get_wall_following_direction():
+
+    #목표지점을 향해 직진
+    def move_toward_goal():    
 
 
 #메인 함수
