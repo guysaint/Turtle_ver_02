b2e72cefa5eb22772ae40487c7b51661f40fa144
guysaint <guysaint@naver.com>
2025-07-22 14:57:25 +0900

    시작점 모양 추가
    기존에 생성된 미로 불러오기 코드 추가
    시작점 모양 화살표, 경로 그리기 추가

--- pyamaze01.py
+++ pyamaze01.py
@@ -1,5 +1,6 @@
 from pyamaze import maze, COLOR, agent
 m = maze(10,10)
 m.CreateMaze(theme=COLOR,white)
-m.CreateMaze(saveMaze=True)
+m.CreateMaze(loadMaze=maze--2025-07-22--14-52-02.csv)
+a=agent(m, shape='arrow', footprints=True)
 m.run()
