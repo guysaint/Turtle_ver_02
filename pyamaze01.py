bde2e92b08ff2d1a19c9e13e8fe54918096fa3fd
guysaint <guysaint@naver.com>
2025-07-22 15:33:01 +0900

    벽 따라가기 스켈레톤 코드
    스켈레톤 코드
    -코드의 뼈대

--- pyamaze01.py
+++ pyamaze01.py
@@ -1,8 +1,28 @@
 from pyamaze import maze, COLOR, agent
-m = maze(10,10)
-#m.CreateMaze(theme=COLOR.light)
-m.CreateMaze(loadMaze="maze--2025-07-22--14-52-02.csv")
+#미로 크기 설정
+MAZE_ROWS = 10
+MAZE_COLS = 10
+m = maze(MAZE_ROWS,MAZE_COLS)
+m.CreateMaze(theme=COLOR.light) # 미로 테마 설정
+#m.CreateMaze(loadMaze="maze--2025-07-22--14-52-02.csv")
 a=agent(m, filled=True, footprints=True)
 m.tracePath({a:m.path})
 m.run()
-print(m.maze_map)
+#벽 따라가기 알고리즘
+
+
+
+#메인 함수
+
+def main():
+    m.run()
+    print(m.maze_map)
+
+#프로그램 실행
+if __name__ =="__main__":
+    main()
+    
+
+
+
+
